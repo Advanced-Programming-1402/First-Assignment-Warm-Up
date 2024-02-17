36328b7193df0bccff47b3e745c772de76b276a5
Mahan Madani <93105700+Mahan-M47@users.noreply.github.com>
2024-02-17 21:39:33 +0330

    added workflows

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -3,19 +3,23 @@ public class Exercises1 {
     implement a function that returns factorial of given n
      */
     public long factorial(int n) {
+        // TODO
         return 0;
     }
+
 
     /*
     implement a function that return nth number of fibonacci series
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
     public long fibonacci(int n) {
+        // TODO
         return 0;
     }
 
+
     /*
-    implement a function that prints a triangle for example:
+    implement a function that returns a triangle for example:
     row = 5
 
     *
@@ -25,9 +29,14 @@ public class Exercises1 {
     *****
 
      */
-
     public char[][] generateTriangle(int rows) {
+        // TODO
         return null;
+    }
+
+
+    public static void main(String[] args) {
+        // test your code here!
     }
 
 }
